b99f4e71ba44d49a36dbcf17dd1f6a269662315c
Orion <orion_larden@hotmail.com>
2017-07-16 20:43:44 +1000

    Remove unused imports

--- src/FlappyIO.java
+++ src/FlappyIO.java
@@ -1,7 +1,5 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.Timer;
--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,11 @@
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
+
 
 public class Game {
 	private ArrayList<Bird> birds;
